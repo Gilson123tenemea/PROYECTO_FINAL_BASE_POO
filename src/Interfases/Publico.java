29b4a358795fe89bcc07bf4bfc9ebd2ddfff9ca4
Gilson123tenemea <tenemeaaguilar@gmail.com>
2024-01-12 12:43:32 -0500

    prueva

--- src/Interfases/Publico.java
+++ src/Interfases/Publico.java
@@ -209,7 +209,7 @@ public class Publico extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
+        System.out.println("Prueva");
     }//GEN-LAST:event_jTextField1ActionPerformed
 //    public void CrearCliente(ObjectContainer base) {
 //
