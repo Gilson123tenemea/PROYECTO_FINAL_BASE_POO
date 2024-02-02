b3f32d8a2934975df5163e03ac6f5ae06ff1ffeb
Gilson123tenemea <tenemeaaguilar@gmail.com>
2024-02-01 20:13:47 -0500

    REPORTE ENCUESTA

--- src/Interfases/Reporte_Encuesta.java
+++ src/Interfases/Reporte_Encuesta.java
@@ -5,32 +5,40 @@
  */
 package Interfases;
 
+import Clases.Encuesta;
 import Clases.RespuestasEncuesta;
 import com.db4o.Db4o;
 import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
+import com.db4o.query.Query;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JOptionPane;
+import javax.swing.RowFilter;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 
 /**
  *
  * @author ADMIN_01
  */
 public class Reporte_Encuesta extends javax.swing.JPanel {
+ private TableRowSorter trs;
+    String encuesta = "", pregunta1 = "", pregunta2 = "", pregunta3 = "", pregunta4 = "", pregunta5 = "";
+    private final int indiceColumnaCodigoEvento = 2;
 
-    /**
-     * Creates new form Reporte_Encuesta
-     */
     public Reporte_Encuesta() {
         initComponents();
         cargarDatosEnTabla();
     }
 
     private void cargarDatosEnTabla() {
-       ObjectContainer base = Db4o.openFile(Inicio.direccion);
-       DefaultTableModel model = (DefaultTableModel) tablares.getModel();
-        model.setRowCount(0); 
+        ObjectContainer base = Db4o.openFile(Inicio.direccion);
+        DefaultTableModel model = (DefaultTableModel) tablares.getModel();
+        model.setRowCount(0);
 
-        ObjectSet<RespuestasEncuesta> result = base.queryByExample(new RespuestasEncuesta(null,null,null,null,null,null,null,null));
+        ObjectSet<RespuestasEncuesta> result = base.queryByExample(new RespuestasEncuesta(null, null, null, null, null, null, null, null));
 
         while (result.hasNext()) {
             RespuestasEncuesta acti = result.next();
@@ -42,17 +50,73 @@ public class Reporte_Encuesta extends javax.swing.JPanel {
                 acti.getRes2(),
                 acti.getRes3(),
                 acti.getRes4(),
-                 acti.getRes5()
-                
+                acti.getRes5()
+
             };
             model.addRow(row);
         }
         base.close();
 
     }
+
+    public void mostrarPreguntas(String codigo) {
+        ObjectContainer base = Db4o.openFile(Inicio.direccion);
+        Query encuestaQuery = base.query();
+        encuestaQuery.constrain(Encuesta.class);
+        encuestaQuery.descend("evento").constrain(codigo);
+        ObjectSet<Encuesta> encuestaResult = encuestaQuery.execute();
+
+        if (!encuestaResult.isEmpty()) {
+            Encuesta encuesta = encuestaResult.next();
+
+            pregunta1 = encuesta.getPregunta1();
+            pregunta2 = encuesta.getPregunta2();
+            pregunta3 = encuesta.getPregunta3();
+            pregunta4 = encuesta.getPregunta4();
+            pregunta5 = encuesta.getPregunta5();
+
+            String mensaje = "Preguntas de la encuesta:\n\n";
+            mensaje += "1. " + pregunta1 + "\n";
+            mensaje += "2. " + pregunta2 + "\n";
+            mensaje += "3. " + pregunta3 + "\n";
+            mensaje += "4. " + pregunta4 + "\n";
+            mensaje += "5. " + pregunta5;
+
+            JOptionPane.showMessageDialog(null, mensaje, "Preguntas de la Encuesta", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "No hay encuesta asociada al evento.", "Sin Encuesta", JOptionPane.WARNING_MESSAGE);
+        }
+
+        base.close();
+    }
+
+    public void mostrarPreguntasSeleccionada() {
+        int filaSeleccionada = tablares.getSelectedRow();
+        if (filaSeleccionada != -1) {
+            String codigoEvento = (String) tablares.getValueAt(filaSeleccionada, indiceColumnaCodigoEvento);
+            mostrarPreguntas(codigoEvento);
+        } else {
+            JOptionPane.showMessageDialog(null, "Selecciona una fila para ver las preguntas.", "Advertencia", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+    
+    public void Filtro() {
+
+        if (cbxbusqueda.getSelectedItem().toString().equalsIgnoreCase("Cedula")) {
+            int Columnastabla = 1;
+            trs.setRowFilter(RowFilter.regexFilter(txtconsulta.getText().trim(), Columnastabla));
+
+        } else if (cbxbusqueda.getSelectedItem().toString().equalsIgnoreCase("Evento")) {
+            int Columnastabla = 2;
+            trs.setRowFilter(RowFilter.regexFilter(txtconsulta.getText().trim(), Columnastabla));
+
+        }
+    }
     
     
     
+    
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -61,6 +125,9 @@ public class Reporte_Encuesta extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tablares = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+        txtconsulta = new javax.swing.JTextField();
+        cbxbusqueda = new javax.swing.JComboBox<>();
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -81,7 +148,25 @@ public class Reporte_Encuesta extends javax.swing.JPanel {
         ));
         jScrollPane1.setViewportView(tablares);
 
-        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 300, 830, 240));
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 290, 830, 240));
+
+        jButton1.setText("Ver eventos");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 240, -1, -1));
+
+        txtconsulta.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtconsultaKeyTyped(evt);
+            }
+        });
+        jPanel1.add(txtconsulta, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 190, 100, -1));
+
+        cbxbusqueda.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccione", "Cedula", "Evento" }));
+        jPanel1.add(cbxbusqueda, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 190, 130, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -95,11 +180,58 @@ public class Reporte_Encuesta extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        mostrarPreguntasSeleccionada();
+
+
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void txtconsultaKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtconsultaKeyTyped
+         if (cbxbusqueda.getSelectedItem().toString().equalsIgnoreCase("Cedula")) {
+
+            txtconsulta.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyReleased(final KeyEvent e) {
+
+                    String cadena = (txtconsulta.getText());
+
+                    txtconsulta.setText(cadena);
+                    Filtro();
+
+                }
+
+            });
+
+        } else if (cbxbusqueda.getSelectedItem().toString().equalsIgnoreCase("Evento")) {
+            txtconsulta.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyReleased(final KeyEvent e) {
+
+                    String cadena = (txtconsulta.getText());
+
+                    txtconsulta.setText(cadena);
+                    Filtro();
+
+                }
+
+            });
+
+        } else if (cbxbusqueda.getSelectedItem().toString().equalsIgnoreCase("Seleccione")) {
+            JOptionPane.showMessageDialog(null, "Debe escoger mediante que campo desea ver los registros");
+        }
+
+        trs = new TableRowSorter(tablares.getModel());
+        tablares.setRowSorter(trs);
+    }//GEN-LAST:event_txtconsultaKeyTyped
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> cbxbusqueda;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tablares;
+    private javax.swing.JTextField txtconsulta;
     // End of variables declaration//GEN-END:variables
 }
